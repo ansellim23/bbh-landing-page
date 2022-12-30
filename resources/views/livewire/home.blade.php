f0995fd8a7e3ed5a725171ba243c090cac4d91cd
Paul Christian De Guzman <ithelpdesk2022143@gmail.com>
2022-12-31 01:52:14 +0800

    added changes

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -3,14 +3,14 @@
     <div class="desktop_view_vid">
         <div class="row justify-content-center">
         <video class="hero-video" playsinline="1" autoplay="1" muted="1" loop="1">
-            <source src="https://hlmcrm.site/assets/BBH_CoverWeb_Desktop.mp4" type="video/mp4">
+            <source src="{{ asset('vids/BBH_Desktop_720.mp4') }}" type="video/mp4">
         </video>
         </div>
     </div>
     <div class="mobile_view_vid">
         <div class="row justify-content-center">
             <video class="hero-video" playsinline="1" autoplay="1" muted="1" loop="1">
-                <source src="https://hlmcrm.site/assets/BBH_CoverWeb_Phone.mp4" type="video/mp4">
+                <source src="{{ asset('vids/BBH_Mobile_480.mp4') }}" type="video/mp4">
             </video>
         </div>
     </div>
